ba64472781e7368d6a1c3b41917f2fd536da074a
Brady <zeromemesdev@gmail.com>
2018-05-10 15:53:31 -0500

    Update AssemblyInfo with new name

--- bettercod/AssemblyInfo.cpp
+++ bettercod/AssemblyInfo.cpp
@@ -4,11 +4,11 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-[assembly:AssemblyTitleAttribute(L"BetterMW3")];
+[assembly:AssemblyTitleAttribute(L"BetterCOD")];
 [assembly:AssemblyDescriptionAttribute(L"")];
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"BetterMW3")];
+[assembly:AssemblyProductAttribute(L"BetterCOD")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2018")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
--- bettercod/resource.h
+++ bettercod/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by bettermw3.rc
+// Used by bettercod.rc
 //
 #define IDI_ICON1                       101
 
